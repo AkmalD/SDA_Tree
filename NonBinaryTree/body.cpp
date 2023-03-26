c87ad5759a924899329c5d191fc86bfdbb4c0cee
AkmalD <117887073+AkmalD@users.noreply.github.com>
2023-03-26 16:47:19 +0700

    Add files via upload

---
+++ NonBinaryTree/body.cpp
@@ -0,0 +1,237 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include "spesifikasi.h"
+
+void Create_tree(Isi_Tree X, int Jml_Node)
+{
+    X[Jml_Node];
+    X[0].info = ' ';
+}
+
+boolean IsEmpty(Isi_Tree P)
+{
+    if (P == NULL)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+void addParent(Isi_Tree P, infotype X)
+{
+    P[1].ps_pr = 0;
+    P[1].ps_nb = 0;
+    P[1].ps_fs = 0;
+    P[1].info = X;
+    printf("%c menjadi parent/root. Parent berada pada Level 0", X);
+    getch();
+    system("cls");
+}
+
+void addChild(Isi_Tree P, infotype X)
+{
+    int idChild, idParent;
+    nbtree pCur;
+    if (!IsEmpty(P)) // child
+    {
+        printf("Pilih index child, masukkan index: ");
+        scanf("%d", &idChild);
+        P[idChild].info = X;
+        printf("Pilih parent, masukkan index: ");
+        scanf("%d", &idParent);
+        P[idChild].ps_pr = idParent;
+        P[idChild].ps_nb = 0;
+        P[idChild].ps_fs = 0;
+        if (P[idParent].ps_fs != 0)
+        {
+            pCur = P[P[idParent].ps_fs];
+            if (P[idChild].ps_pr == pCur.ps_pr)
+            {
+                P[P[idParent].ps_fs].ps_nb = idChild;
+            }
+        }
+        else
+        {
+            P[idParent].ps_fs = idChild;
+        }
+    }
+    else
+    {
+        printf("Belum ada parent!");
+        return;
+    }
+    getch();
+    system("cls");
+}
+
+void PostOrder(Isi_Tree P)
+{
+    int pCur = 1;
+    bool resmi = true;
+
+    while (pCur != 0)
+    {
+        if (P[pCur].ps_fs != 0 && resmi)
+        {
+            pCur = P[pCur].ps_fs;
+        }
+        else
+        {
+            fflush(stdin);
+            printf("%c ", P[pCur].info);
+            if (P[pCur].ps_nb != 0)
+            {
+                pCur = P[pCur].ps_nb;
+                resmi = true;
+            }
+            else
+            {
+                pCur = P[pCur].ps_pr;
+                resmi = false;
+            }
+        }
+    }
+    printf("\n");
+}
+
+void PreOrder(Isi_Tree P)
+{
+    int Pcur;   // Posisi Node Aktif
+    bool Resmi; // Sifat kunjungan resmi
+    Pcur = 1;
+    printf("%c", P[Pcur].info);
+    Resmi = true;
+    do
+    {
+        if (P[Pcur].ps_fs != 0 && Resmi)
+        {
+            Pcur = P[Pcur].ps_fs;
+            printf("%c", P[Pcur].info);
+        }
+        else if (P[Pcur].ps_nb != 0)
+        {
+            Pcur = P[Pcur].ps_nb;
+            printf("%c", P[Pcur].info);
+            Resmi = true;
+        }
+        else
+        {
+            Pcur = P[Pcur].ps_pr;
+            Resmi = false;
+        }
+    } while ((P[Pcur].ps_pr != 0));
+    printf("\n");
+}
+
+void InOrder(Isi_Tree P)
+{
+    int Pcur;   // Posisi Node Aktif
+    bool Resmi; // Sifat kunjungan resmi
+    Pcur = 1;
+    Resmi = true;
+    while (Pcur != 0)
+    {
+        if (P[Pcur].ps_fs != 0 && Resmi)
+        {
+            Pcur = P[Pcur].ps_fs;
+        }
+        else
+        {
+            if (Resmi)
+            {
+                printf("%c", P[Pcur].info);
+            }
+
+            if (Pcur == P[P[Pcur].ps_pr].ps_fs)
+            {
+                printf("%c", P[P[Pcur].ps_pr].info);
+            }
+
+            if (P[Pcur].ps_nb != 0)
+            {
+                Pcur = P[Pcur].ps_nb;
+                Resmi = true;
+            }
+            else
+            {
+                Pcur = P[Pcur].ps_pr;
+                Resmi = false; // Numpang lewat
+            }
+        }
+    }
+    printf("\n");
+}
+
+boolean Search(Isi_Tree P, infotype X, int Jmlh)
+{
+    if (IsEmpty(P))
+    {
+        return false;
+    }
+    else
+    {
+        address idx = 1;
+        while (idx <= Jmlh && P[idx].info != X)
+        {
+            if (P[idx].ps_fs != 0)
+            {
+                idx = P[idx].ps_fs;
+            }
+            else
+            {
+                idx = P[idx].ps_nb;
+            }
+        }
+        if (idx <= Jmlh)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+}
+
+void insQueue(Queue *q, int val)
+{
+    q->data[++q->rear] = val;
+}
+
+int delQueue(Queue *q)
+{
+    return q->data[++q->front];
+}
+
+void Level_order(Isi_Tree X, int jmlh)
+{
+    int Pcur = 1;
+    Queue Q;
+    Q.front = Q.rear = -1;
+    printf("%d ", X[1].info);
+    Pcur = X[1].ps_fs;
+    insQueue(&Q, 0);
+    for(int i = 0; i<jmlh; i++){
+        while (Pcur != 0)
+        {
+            printf("%d ", X[Pcur].info);
+
+            if (X[Pcur].ps_fs != 0)
+            {
+                insQueue(&Q, X[Pcur].ps_fs);
+            }
+
+            if (X[Pcur].ps_nb != 0)
+            {
+                Pcur = X[Pcur].ps_nb;
+            }
+            else
+            {
+                Pcur = delQueue(&Q);
+            }
+        }
+    }
+}
---
+++ NonBinaryTree/main.cpp
@@ -0,0 +1,74 @@
+#include <stdio.h>
+#include "body.cpp"
+
+int main()
+{
+
+    Isi_Tree tree1;
+    infotype isi, Cari;
+    int pilih, jmlNode;
+
+    fflush(stdin);
+    printf("Berapa jumlah node yang akan dibuat : ");
+    scanf("%d", &jmlNode);
+
+    Create_tree(tree1, jmlNode);
+    fflush(stdin);
+    printf("Tambahkan parent: ");
+    scanf("%c", &isi);
+
+    addParent(tree1, isi);
+
+    while (true)
+    {
+        fflush(stdin);
+        printf("1. Insert\n2. Search\n3. Traversal\n4. Delete\n5. Keluar\nPilih: ");
+        scanf("%d", &pilih);
+        switch (pilih)
+        {
+        case 1:
+            fflush(stdin);
+            printf("Masukkan karakter/isi: ");
+            scanf("%c", &isi);
+            addChild(tree1, isi);
+            break;
+        case 2:
+            printf("Masukkan Data Yang ingin anda cari: ");
+            scanf(" %c", &Cari);
+            if (Search(tree1, Cari, jmlNode))
+            {
+                printf("true\n");
+            }
+            else
+            {
+                printf("false\n");
+            }
+            break;
+        case 3:
+            printf("Pilih yang ingin anda lakukan:\n1. Pre-Order\n2. Post-Order\n3. In-Order\n4. Level-Order\n");
+            printf("Input: ");
+            scanf("%d",&pilih);
+            switch (pilih)
+            {
+                case 1:
+                    PreOrder(tree1);
+                    break;
+                case 2:
+                    PostOrder(tree1);
+                    break;
+                case 3:
+                    InOrder(tree1);
+                    break;
+                case 4:
+                    Level_order(tree1, jmlNode);
+                    break;
+            }
+        case 4:
+            break;
+        case 5:
+            return 0;
+            break;
+        }
+    }
+    return 0;
+}
